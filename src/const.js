0b06a810c2f7c0a011f121f4e2b6e6cfc9df6910
CLXD <lalobolito196@gmail.com>
2024-05-06 16:54:48 -0600

    Add: Add compatibility and tags to the project list Added PC and mobile support functionality to the project gallery. I also removed the "learnings" list and added compatibility tags to each project to provide users with a quick understanding of each project's compatibility.

--- src/const.js
+++ src/const.js
@@ -4,7 +4,7 @@ export const PROJECTS = [
     title: "Falling Sand",
     description:
       "Una simulación de arena que cae y se acumula en una cuadrícula, permitiendo a los usuarios experimentar con diferentes efectos.",
-    learnings: ["Eventos de mouse y touch", "DOM", "Simulación de partículas"],
+    compatibility: { pc: true, mobile: true },
     theme: { isDark: true },
   },
   {
@@ -12,11 +12,7 @@ export const PROJECTS = [
     title: "Elementary Cellular Automata",
     description:
       "Una implementación de autómatas celulares elementales, donde cada celda evoluciona según reglas simples basadas en el estado de sus vecinos.",
-    learnings: [
-      "Concepto de autómatas celulares",
-      "Reglas de evolución",
-      "Manipulación del DOM",
-    ],
+    compatibility: { pc: true, mobile: true },
     theme: { isDark: false },
   },
   {
@@ -24,11 +20,7 @@ export const PROJECTS = [
     title: "Game of Life",
     description:
       "Explora la fascinante simulación de la evolución y la supervivencia en el clásico Juego de la Vida. ¡Observa cómo las formas de vida se desarrollan y evolucionan ante tus ojos!",
-    learnings: [
-      "Reglas del juego de la vida",
-      "Implementación de algoritmos de evolución",
-      "Optimización del rendimiento",
-    ],
+    compatibility: { pc: true, mobile: true },
     theme: { isDark: false },
   },
   {
@@ -36,11 +28,7 @@ export const PROJECTS = [
     title: "Breakout",
     description:
       "¡Llega el nuevo juego de Breakout, Desafía tus habilidades y reflejos mientras rompes ladrillos con tu paleta. ¡Prepárate para horas de diversión con este emocionante desafío de habilidad!",
-    learnings: [
-      "Ángulos",
-      "Movimiento con teclado",
-      "uso de canvas sin un grid",
-    ],
+    compatibility: { pc: true, mobile: true },
     theme: { isDark: false },
   },
   {
@@ -48,7 +36,7 @@ export const PROJECTS = [
     title: "Space Invaders",
     description:
       "¡Prepárate para la batalla del siglo con Space Invaders! Defiende la Tierra de la invasión alienígena en este emocionante juego de disparos. ¡Destruye a los invasores antes de que sea demasiado tarde y conviértete en el héroe de la humanidad! ¿Tienes lo necesario para salvar el planeta?",
-    learnings: ["clases", "Movimiento con teclado", "POO"],
+    compatibility: { pc: true, mobile: false },
     theme: { isDark: false },
   },
   {
@@ -56,7 +44,15 @@ export const PROJECTS = [
     title: "Tetris",
     description:
       "¡Domina la velocidad y la estrategia en este electrizante desafío de bloques en caída! Tetris: ¡el fenómeno de puzzle que te mantendrá pegado a la pantalla por horas de diversión sin parar!",
-    learnings: ["clases", "Movimiento con teclado", "POO"],
+    compatibility: { pc: true, mobile: true },
+    theme: { isDark: false },
+  },
+  {
+    slug: "07-snake",
+    title: "Snake",
+    description:
+      "Dirige a la serpiente a través del campo de juego mientras se alimenta. Pero ten cuidado, Evita chocar contra las paredes o contra tu propia cola.",
+    compatibility: { pc: true, mobile: false },
     theme: { isDark: false },
   },
 ];
