1d45e9be50559793ff7ae0c85c4d98e454c6ed7c
CLXD <lalobolito196@gmail.com>
2024-08-10 22:28:12 -0600

    Setup Tailwind with custom colors, dark mode, and fonts.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: ["class", "[data-theme='dark']"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "var(--r-black, #000)",
+        light: "var(--r-white, #fff)",
+        "r-red": "var(--r-red, #e22f2e)",
+        "r-orange": "var(--r-orange, #dc6d2c)",
+        "r-yellow": "var(--r-yellow, #ddb631)",
+      },
+      fontFamily: {
+        krypton: ["Krypton", "ui-monospace", "Consolas", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
